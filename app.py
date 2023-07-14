78e572f8f95905190e9aec618f4d2bdd54e35205
Delaram Golpayegani <sgolpays@tcd.ie>
2023-07-14 12:12:37 +0100

    update

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from website import create_app
 
 app = create_app()
-app.secret_key = "LDNh56g77**hjh(u)"
+app.secret_key = "abcde"
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
